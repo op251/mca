16463faa2f06d146d1fc7356e55e7f09cbb0da4b
op251 <127918951+op251@users.noreply.github.com>
2024-10-01 15:04:55 +0530

    Update k_ex7.java

--- k_ex7.java
+++ k_ex7.java
@@ -24,3 +24,112 @@ public class k_ex7{
         System.out.println("The maximum number is: " + max);
     }
 }
+
+
+3
+
+4
+
+5
+
+6
+
+7
+
+8
+
+#include<stdio.h>
+
+#include<conio.h>
+
+void main() {
+
+int a[5]= (1, 2, 3, 4, 5}; int b[5] = \{4, 5, 6, 7, 8\} * tau
+
+int c[10], 1, j, k, flag, count = 5;
+
+clrscr();
+
+9
+
+10
+
+11
+
+c[1] = a[1] ; for (i=0; i < 5; i++) {
+
+12
+
+13
+
+14日
+
+for ( j=8.j< 5j ++)( flag = :
+
+15
+
+16
+
+17
+
+18
+
+19
+
+for (k;k< count; k++) { if (b[j] = e[k]) ( flag- break:
+
+20
+
+21
+
+22
+
+23
+
+24
+
+25
+
+}
+
+if (flag= 0 ) count++; c[count]= b[j]
+
+26
+
+27
+
+}
+
+28
+
+29
+
+30
+
+31
+
+白
+
+printf("Union of CHO GREEC for ( k = 0 k < count; k++)
+
+32
+
+33
+
+printf("id", [k]):
+
+34
+
+35
+
+getch();
+
+36
+
+37
+
+C source file
+
+Type here to search
+
+lenc
